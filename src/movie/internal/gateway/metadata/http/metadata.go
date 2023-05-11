ae46f121955ffc0945695e58e6269487c3ef8b94
SeungBin Kim <luxorienskim@gmail.com>
2023-05-11 15:27:47 +0900

    feat: gateway Get function

--- src/movie/internal/gateway/metadata/http/metadata.go
+++ src/movie/internal/gateway/metadata/http/metadata.go
@@ -1,5 +1,14 @@
 package http
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	model "microservices/metadata/pkg"
+	"microservices/movie/internal/gateway"
+	"net/http"
+)
+
 // Gateway defines a movie metadata HTTP gateway.
 type Gateway struct {
 	addr string
@@ -8,4 +17,31 @@ type Gateway struct {
 // New creates a new HTTP gateway for a movie metadata service.
 func New(addr string) *Gateway {
 	return &Gateway{addr}
+}
+
+// Get gets movie metadata by a movie id.
+func (g *Gateway) Get(ctx context.Context, id string) (*model.Metadata, error) {
+	request, err := http.NewRequest(http.MethodGet, g.addr+"/metadata", nil)
+	if err != nil {
+		return nil, err
+	}
+	request = request.WithContext(ctx)
+	values := request.URL.Query()
+	values.Add("id", id)
+	request.URL.RawQuery = values.Encode()
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	if response.StatusCode == http.StatusNotFound {
+		return nil, gateway.ErrNotFound
+	} else if response.StatusCode/100 != 2 {
+		return nil, fmt.Errorf("non-2xx response: %v", response)
+	}
+	var v *model.Metadata
+	if err := json.NewDecoder(response.Body).Decode(&v); err != nil {
+		return nil, err
+	}
+	return v, err
 }
