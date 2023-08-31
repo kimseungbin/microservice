8ef369e267763411996d45f2adcd4d321329c9b9
SeungBin Kim <luxorienskim@gmail.com>
2023-08-31 14:15:12 +0900

    update: implement service discovery in rating main

--- src/rating/cmd/main.go
+++ src/rating/cmd/main.go
@@ -1,18 +1,47 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log"
+	"microservices/pkg/discovery"
+	"microservices/pkg/discovery/consul"
 	"microservices/rating/internal/controller/rating"
 	httphandler "microservices/rating/internal/handler/http"
 	"microservices/rating/internal/repository/memory"
 	"net/http"
+	"time"
 )
 
+const serviceName = "rating"
+
 func main() {
-	log.Println("Starting the rating service")
+	var port int
+	flag.IntVar(&port, "port", 8082, "API handler port")
+	flag.Parse()
+	log.Println("Starting the rating service on port %d", port)
+	registry, err := consul.NewRegistry("localhost:8500")
+	if err != nil {
+		return
+	}
+	ctx := context.Background()
+	instanceID := discovery.GenerateInstanceID(serviceName)
+	if err := registry.Register(ctx, instanceID, serviceName, fmt.Sprintf("localhost:%d", port)); err != nil {
+		panic(err)
+	}
+	go func() {
+		for {
+			if err := registry.ReportHealthyState(instanceID, serviceName); err != nil {
+				log.Println("Failed to report healthy state: " + err.Error())
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	defer registry.Deregister(ctx, instanceID, serviceName)
 	repo := memory.New()
-	ctrl := rating.New(repo)
-	h := httphandler.New(ctrl)
+	svc := rating.New(repo)
+	h := httphandler.New(svc)
 	http.Handle("/rating", http.HandlerFunc(h.Handle))
 	if err := http.ListenAndServe(":8082", nil); err != nil {
 		panic(err)
