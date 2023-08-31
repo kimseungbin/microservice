10f1dc7b2847bf66ee56cccb66bb4304ab2bf5dc
SeungBin Kim <luxorienskim@gmail.com>
2023-08-31 14:23:13 +0900

    update: implement service discovery in movie main

--- src/movie/cmd/main.go
+++ src/movie/cmd/main.go
@@ -1,20 +1,49 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log"
 	"microservices/movie/internal/controller/movie"
 	metadataGateway "microservices/movie/internal/gateway/metadata/http"
 	ratingGateway "microservices/movie/internal/gateway/rating/http"
 	httpHandler "microservices/movie/internal/handler/http"
+	"microservices/pkg/discovery"
+	"microservices/pkg/discovery/consul"
 	"net/http"
+	"time"
 )
 
+const serviceName = "movie"
+
 func main() {
-	log.Printf("Starting the movie service")
-	metadataGw := metadataGateway.New("localhost:8081")
-	ratingGw := ratingGateway.New("localhost:8082")
-	controller := movie.New(ratingGw, metadataGw)
-	handler := httpHandler.New(controller)
+	var port int
+	flag.IntVar(&port, "port", 8083, "API handler port")
+	flag.Parse()
+	log.Printf("Starting the movie service on port %d", port)
+	registry, err := consul.NewRegistry("localhost:8500")
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.Background()
+	instanceID := discovery.GenerateInstanceID(serviceName)
+	if err := registry.Register(ctx, instanceID, serviceName, fmt.Sprintf("localhost:%d", port)); err != nil {
+		panic(err)
+	}
+	go func() {
+		for {
+			if err := registry.ReportHealthyState(instanceID, serviceName); err != nil {
+				log.Println("Failed to report healthy state: " + err.Error())
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	defer registry.Deregister(ctx, instanceID, serviceName)
+	metadataGateway := metadataGateway.New(registry)
+	ratingGateway := ratingGateway.New(registry)
+	svc := movie.New(ratingGateway, metadataGateway)
+	handler := httpHandler.New(svc)
 	http.Handle("/movie", http.HandlerFunc(handler.GetMovieDetails))
 	if err := http.ListenAndServe(":8083", nil); err != nil {
 		panic(err)
